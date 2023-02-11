fda780f94a8c8a3261500e84b54fc311f7aa6fb2
ScaredCube <74418739+ScaredCube@users.noreply.github.com>
2023-02-11 15:13:16 +0800

    push

--- index.py
+++ index.py
@@ -24,55 +24,52 @@ except:
 bv='BV1qd4y1h7na'
 
 
+async def download_url(url: str, out: str, info: str):
+    # 下载函数
+    async with httpx.AsyncClient(headers=HEADERS) as sess:
+        resp = await sess.get(url)
+        length = resp.headers.get('content-length')
+        with open(out, 'wb') as f:
+            process = 0
+            for chunk in resp.iter_bytes(1024*2):
+                if not chunk:
+                    break
+
+                process += len(chunk)
+                print(f'下载 {info} {process} / {length}')
+                f.write(chunk)
 async def main():
     v = video.Video(bvid=bv, credential=credential)
     print('正在获取url...')
-    url = await v.get_download_url(0)
+    # 获取视频下载链接
+    download_url_data = await v.get_download_url(0)
+    # 解析视频下载信息
+    detecter = video.VideoDownloadURLDataDetecter(data=download_url_data)
+    streams = detecter.detect_best_streams()
+    #视频标题
     ti=await v.get_info()
     ti=ti['title']
     if '/' in ti:
         ti=ti.replace('/',',')
-    # 视频轨链接
-    video_url = url["dash"]["video"][0]['baseUrl']
-    # 音频轨链接
-    audio_url = url["dash"]["audio"][0]['baseUrl']
-    async with ClientSession() as sess:
-        # 下载视频流
-        async with sess.get(video_url, headers=HEADERS) as resp:
-            length = resp.headers.get('content-length')
-            with open('video_temp.m4s', 'wb') as f:
-                process = 0
-                while True:
-                    chunk = await resp.content.read(4096*4)
-                    if not chunk:
-                        break
-                    process += len(chunk)
-                    per=int(process)/int(length)
-                    perc=format(per,'.2%')
-                    print(f'下载视频流 {process} / {length} ({perc})')
-                    f.write(chunk)
-
-        # 下载音频流
-        async with sess.get(audio_url, headers=HEADERS) as resp:
-            length = resp.headers.get('content-length')
-            with open('audio_temp.m4s', 'wb') as f:
-                process = 0
-                while True:
-                    chunk = await resp.content.read(4096*4)
-                    if not chunk:
-                        break
-                    process += len(chunk)
-                    per=int(process)/int(length)
-                    perc=format(per,'.2%')
-                    print(f'下载音频流 {process} / {length} ({perc})')
-                    f.write(chunk)
+    # 有 MP4 流 / FLV 流两种可能
+    if detecter.check_flv_stream() == True:
+        # FLV 流下载
+        await download_url(streams[0].url, "flv_temp.flv", "FLV 音视频流")
+        # 转换文件格式
+        os.system(f'{FFMPEG_PATH} -i flv_temp.flv {ti}.mp4')
+        # 删除临时文件
+        os.remove("flv_temp.flv")
+    else:
+        # MP4 流下载
+        await download_url(streams[0].url, "video_temp.m4s", "视频流")
+        await download_url(streams[1].url, "audio_temp.m4s", "视频流")
         # 混流
         print('混流中')
         os.system(f'{FFMPEG_PATH} -i video_temp.m4s -i audio_temp.m4s -vcodec copy -acodec copy {ti}.mp4')
         # 删除临时文件
         os.remove("video_temp.m4s")
         os.remove("audio_temp.m4s")
-        print(f'已下载为: {ti}.mp4')
+    print(f'已下载为: {ti}.mp4')
 
 table='fZodR9XQDSUm21yCkr6zBqiveYah8bt4xsWpHnJE7jL5VG3guMTKNPAwcF'
 tr={}
@@ -143,18 +140,38 @@ def down_ss():
         bvid=bangumi.Episode(i['id']).get_bvid()
         bv=bvid
         asyncio.get_event_loop().run_until_complete(main())
+
+def pic():
+    global bv
+    inv=input("请输入AV|BV|EP号 (必须以大写AV|BV|EP开头): ")
+    if inv[:2]=='AV':
+        print(enc(int(inv[2:])))
+        bv=enc(int(inv[2:]))
+    elif inv[:2]=='EP':
+        ep=bangumi.Episode(int(inv[2:]))
+        print(ep.get_bvid())
+        bv=ep.get_bvid()
+    elif inv[:2]=='BV':
+        bv=inv
+    else:
+        print('请正确输入')
+        return
+    v_pic = video.Video(bvid=bv, credential=credential)
+    print('封面链接:',sync(v_pic.get_info())['pic'])
     
 
 
-print("1.下载视频\n2.番剧批量下载\n3.设置ffmpeg路径 (默认为环境变量)\n4.登录\n")
+print("1.下载视频\n2.番剧批量下载\n3.提取视频封面\n4.设置ffmpeg路径 (默认为环境变量)\n5.登录\n")
 cho=input('请输入序号: ')
 if cho=='1':
     down()
 elif cho=='2':
     down_ss()
 elif cho=='3':
-    ffp()
+    pic()
 elif cho=='4':
+    ffp()
+elif cho=='5':
     lo()
 
 input('按回车退出')
